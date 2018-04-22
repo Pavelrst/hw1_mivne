3effd2f6c87e3f4da9e6b61cd83450be626d034b
Uri <sukirstn@campus.technion.ac.il>
2018-04-22 21:36:27 +0300

    most of it

--- CompArch-hw1/sim_core.cpp
+++ CompArch-hw1/sim_core.cpp
@@ -9,13 +9,16 @@ typedef struct pipe_level {
     PipeStageState my_pipe_state; //Struct, defined in sim_api.h
     int32_t command_address;
     int32_t alu_result;
-
+    bool branch_taken;
+    int32_t memory_data;
     //If you add new fields, remember to reset them in SIM_CoreReset and in advancePipe!!!!!!
 }pipeLevel;
 
 static int32_t PC;
 static int32_t regFile[SIM_REGFILE_SIZE];
 pipeLevel pipe[SIM_PIPELINE_DEPTH];
+bool isHalt;
+bool waiting_for_memory;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////Core reset/////////////////////////////////////////////////////////////
@@ -31,11 +34,17 @@ pipeLevel pipe[SIM_PIPELINE_DEPTH];
 */
 int SIM_CoreReset(void) {
     PC = 0;
+    isHalt = false;
+    waiting_for_memory = false;
+
     for(int i=0; i<SIM_REGFILE_SIZE; i++)//Resetting registers
         regFile[i] == 0;
     for(int i=0; i<SIM_PIPELINE_DEPTH; i++){//Resetting the pipe
         pipe[i].alu_result = 0;
         pipe[i].command_address = 0;
+        pipe[i].branch_taken = false;
+        pipe[i].memory_data = 0;
+
         //Resetting the pipe state
         pipe[i].my_pipe_state.src1Val = 0;
         pipe[i].my_pipe_state.src2Val = 0;
@@ -55,9 +64,14 @@ int SIM_CoreReset(void) {
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void advancePipe(){
+    if(waiting_for_memory == true){
+        return;
+    }
     for(int i=SIM_PIPELINE_DEPTH-1; i>0; i--){
         pipe[i].alu_result = pipe[i-1].alu_result;
+        pipe[i].branch_taken = pipe[i-1].branch_taken;
         pipe[i].command_address = pipe[i-1].command_address;
+        pipe[i].memory_data = pipe[i-1].memory_data;
         //updating pipe stage state struct
         pipe[i].my_pipe_state.src1Val = pipe[i-1].my_pipe_state.src1Val;
         pipe[i].my_pipe_state.src2Val = pipe[i-1].my_pipe_state.src2Val;
@@ -72,14 +86,124 @@ void advancePipe(){
 }
 
 void fetch(){
-    pipe[0].command_address = pc;
+    if(isHalt){
+      pipe[FETCH].command_address = -1;//NOP, not read from instruction memory
+    }else{
+        pipe[FETCH].command_address = pc;
+    }
+    pipe[FETCH].branch_taken = false;
+    pipe[FETCH].alu_result = 0; //ours, not necessary
+    pipe[FETCH].memory_data = 0; // ours, not necessary
+
+    //reset all printed fields - in struct pipeStageState
+    pipe[FETCH].my_pipe_state.src1Val = 0;
+    pipe[FETCH].my_pipe_state.src2Val = 0;
+    //All fields in the command are reset below
+    pipe[FETCH].my_pipe_state.cmd.opcode = CMD_NOP;
+    pipe[FETCH].my_pipe_state.cmd.dst = 0;
+    pipe[FETCH].my_pipe_state.cmd.src1 = 0;
+    pipe[FETCH].my_pipe_state.cmd.src2 = 0;
+    pipe[FETCH].my_pipe_state.cmd.isSrc2Imm = 0;
+
     pc += 4;
 }
 
-void decode(){}
+void decode(){
+    if(!isHalt){
+        SIM_MemInstRead(pipe[DECODE].command_address, &pipe[1].my_pipe_state.cmd);
+    }else{
+      pipe[DECODE].my_pipe_state.cmd.opcode = CMD_NOP;
+    }
+}
 
-void execute(){}
-void memory(){}
+void execute(){
+    int dest = pipe[EXECUTE].my_pipe_state.cmd.dst;
+    int src1 = pipe[EXECUTE].my_pipe_state.cmd.src1;
+    int src2 = pipe[EXECUTE].my_pipe_state.cmd.src2;
+
+    switch(pipe[EXECUTE].my_pipe_state.cmd.opcode){
+        case CMD_NOP:
+            return;
+        case CMD_ADD:
+            assert(pipe[EXECUTE].my_pipe_state.cmd.isSrc2Imm == false);
+            regFile[dest] = regFile[src1] + regFile[src2];
+            break;
+        case CMD_SUB:
+            assert(pipe[EXECUTE].my_pipe_state.cmd.isSrc2Imm == false);
+            regFile[dest] = regFile[src1] - regFile[src2];
+            break;
+        case CMD_ADDI:
+            assert(pipe[EXECUTE].my_pipe_state.cmd.isSrc2Imm == true);
+            regFile[dest] = regFile[src1] + src2;
+            break;
+        case CMD_SUBI:
+            assert(pipe[EXECUTE].my_pipe_state.cmd.isSrc2Imm == true);
+            regFile[dest] = regFile[src1] - src2;
+            break;
+        case CMD_LOAD:
+            if(pipe[EXECUTE].my_pipe_state.cmd.isSrc2Imm == true){
+                pipe[EXECUTE].alu_result = regFile[src1] + src2;
+            }else{
+                pipe[EXECUTE].alu_result = regFile[src1] + regFile[src2];
+            }
+            break;
+        case CMD_STORE:
+            if(pipe[EXECUTE].my_pipe_state.cmd.isSrc2Imm == true){
+                pipe[EXECUTE].alu_result = regFile[dest] + src2;
+            }else{
+                pipe[EXECUTE].alu_result = regFile[dest] + regFile[src2];
+            }
+            break;
+        case CMD_BR:
+            pipe[EXECUTE].alu_result = (pc - 4) + regFile[dest];//-4 because we advanced PC too many times
+            //PC is update at MEM stage of the pipe
+            break;
+        case CMD_BREQ:
+            assert(pipe[2].my_pipe_state.cmd.isSrc2Imm == false);
+            if(regFile[src1] == regFile[src2]){
+                pipe[EXECUTE].branch_taken = true;
+            }else{
+                pipe[EXECUTE].branch_taken = false;
+            }
+            pipe[EXECUTE].alu_result = (pc - 4) + regFile[dest];//-4 because we advanced PC too many times
+            break;
+        case CMD_BRNEQ:
+            assert(pipe[2].my_pipe_state.cmd.isSrc2Imm == false);
+            if(regFile[src1] != regFile[src2]){
+                pipe[EXECUTE].branch_taken = true;
+            }else{
+                pipe[EXECUTE].branch_taken = false;
+            }
+            pipe[EXECUTE].alu_result = (pc - 4) + regFile[dest];//-4 because we advanced PC too many times
+            break;
+        case CMD_HALT:
+            isHalt = true;
+            break;
+        default:
+            printf("Command has no legal opcode!!");
+    }
+}
+
+void memory(){
+    uint32_t memory_address = (uint32_t)pipe[MEMORY].alu_result;
+    switch(pipe[MEMORY].my_pipe_state.cmd.opcode){
+        case CMD_LOAD:
+            //Memory may wait multiple cycles before giving result
+            if(SIM_MemDataRead(memory_address, &pipe[MEMORY].memory_data) != 0){
+                waiting_for_memory = true;
+            }else{
+                waiting_for_memory = false;
+            }
+            break;
+        case CMD_STORE:
+            int32_t reg_index = pipe[MEMORY].my_pipe_state.cmd.src1;
+            SIM_MemDataWrite(memory_address, regFile[reg_index]);
+            break;
+        default:
+            //not a memory command
+            break;
+    }
+}
 void writeback(){}
 
 void hazard_detect(){}
